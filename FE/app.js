010441ae2c6bedef67cd067304ff311ffee22185
Devin Arnold <dbarnold321@gmail.com>
2019-07-29 12:44:00 -0600

    added dynamic rednering to card

--- FE/app.js
+++ FE/app.js
@@ -1,6 +1,9 @@
 addEventListener('DOMContentLoaded', (event)=> console.log("Dom Fully Loaded and Parsed"))
 
-
+const cardTitle = document.querySelector(".card-title")
+const imageSrc = document.querySelector(".card-img-top")
+const cardSubtitle = document.querySelector(".card-subtitle")
+const cardText = document.querySelector(".card-text")
 
 artAPI = "https://collectionapi.metmuseum.org/public/collection/v1/objects/436535"
 
@@ -11,7 +14,11 @@ fetch(artAPI)
 function parseJSON(artAPI){
     return artAPI.json()
 }
+function createCards(artAPI){
+    cardTitle.innerText = artAPI.title
+    imageSrc.src = artAPI.primaryImage
+    cardSubtitle.innerText = artAPI.artistDisplayName
+    cardText.innerText = artAPI.artistDisplayBio
 
-function createCards(response){
-    console.log(response)
+    console.log(artAPI)
 }
