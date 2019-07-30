40f330ae686e2b9dd5c951ca4ee00b6cc1f6b7a7
Devin Arnold <dbarnold321@gmail.com>
2019-07-29 20:15:40 -0600

    added api refactoring

--- FE/app.js
+++ FE/app.js
@@ -1,24 +1,68 @@
 addEventListener('DOMContentLoaded', (event)=> console.log("Dom Fully Loaded and Parsed"))
 
-const cardTitle = document.querySelector(".card-title")
-const imageSrc = document.querySelector(".card-img-top")
-const cardSubtitle = document.querySelector(".card-subtitle")
-const cardText = document.querySelector(".card-text")
 
-artAPI = "https://collectionapi.metmuseum.org/public/collection/v1/objects/436535"
+const container = document.querySelector(".card")
+const searchbutton = document.querySelector(".searchButton")
+const searchTerm = document.querySelector(".searchTerm")
 
-fetch(artAPI)
-.then(parseJSON)
-.then(createCards)
+searchbutton.addEventListener('click',event =>{
+    const searchItem = searchTerm.value
+    searchApi(searchItem)
+})
 
+function searchApi(searchItem){
+   // console.log(searchItem)
+    stringApi = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchItem}`
+    fetch(stringApi)
+    .then(parseJSON)
+    .then(iterateAPI)
+}
 function parseJSON(artAPI){
     return artAPI.json()
-}
-function createCards(artAPI){
-    cardTitle.innerText = artAPI.title
-    imageSrc.src = artAPI.primaryImage
-    cardSubtitle.innerText = artAPI.artistDisplayName
-    cardText.innerText = artAPI.artistDisplayBio
+    }
 
-    console.log(artAPI)
+    function CreateAPICalls(artAPI){
+        artAPI.forEach(element => {createAPIString(element)
+        });  
+    }
+
+    function createAPIString(element){
+        artAPI = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`
+        console.log(artAPI)
+        APIcall(artAPI)
+    }
+
+    function APIcall(artAPI){
+    fetch(artAPI)
+    .then(parseJSON)
+    .then(CreateCard)
+    }
+
+    function iterateAPI(result){
+        console.log(result)
+        const idArray = result.objectIDs.map(x => {createAPIString(x)
+    }
+    )}
+    function CreateCard(artAPI){
+
+        container1 = document.createElement("div")
+        container.appendChild(container1)
+
+        img = document.createElement("img")
+        img.src = artAPI.primaryImage
+        container1.appendChild(img)
+
+        h3 = document.createElement("h3")
+        h3.innerText = artAPI.title
+        container1.appendChild(h3)
+
+        h4 = document.createElement("h4")
+        h4.innerText = artAPI.artistDisplayName
+        container1.appendChild(h4)
+
+        p = document.createElement("p")
+        p.innerText = artAPI.artistDisplayName
+        container1.appendChild(p)
 }
+
+
